54620670a0c732594a8e4079653f52226c1bd689
fatcasper <yhjclj@yeah.net>
2020-10-07 15:21:18 +0800

    Initial Commit

---
+++ learing git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learing git
+//
+//  Created by 魏碧荣 on 2020/10/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
